8d0515d24997a2a602824a3b449556a7d187d371
AbdulazizRus <coderdim@gmail.com>
2024-07-25 20:55:38 +0500

    bot

---
+++ bot/bot.js
@@ -0,0 +1,7 @@
+const TELEGRAM_BOT = require('node-telegram-bot-api')
+
+const bot = new TELEGRAM_BOT(process.env.TOKEN, { polling: true })
+
+bot.on('message', (msg) => {
+  console.log(msg.text)
+})
--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const mongoose = require('mongoose')
 const app = express()
 require('dotenv').config()
 
 // MIDDLEWARE
 app.use(express.json())
 
+// REQUIRE
+require('./bot/bot')
+
 async function dev() {
   try {
-    mongoose
+    await mongoose
       .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
       .then(() => console.log('Mongodb Successfully Connected'))
       .catch((err) => console.log(err))
+    app.listen(process.env.PORT, () => {
+      console.log(`Server STARTED https://localhost:${process.env.PORT}`)
+    })
   } catch (error) {
     console.log(error)
   }
