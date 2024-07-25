4462cad02e2fd2b10a820d0fdbf4a7444bcfa63c
AbdulazizRus <coderdim@gmail.com>
2024-07-25 08:25:49 +0500

    started project

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+app.use(express.json())
+
+const PORT = 3000
+const MONGO_URI =
+  'mongodb+srv://coderdim:Cgr2xjwfwNuOpblm@texno.cauaiax.mongodb.net/?retryWrites=true&w=majority&appName=texno'
+
+async function dev() {
+  try {
+    await mongoose
+      .connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+      .then(() => console.log('MongoDB connected✔✔✔✔'))
+      .catch((err) => console.log('MONGO ULANMADI✖✖✖', err))
+
+    app.listen(PORT, () => console.log(`SERVER idhga tusdi: ${`http://localhost:${PORT}`}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+dev()
